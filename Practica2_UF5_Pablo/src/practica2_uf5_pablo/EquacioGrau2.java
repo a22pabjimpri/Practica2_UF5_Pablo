887542fd2c9b4949a8092b420de0b17228b1395b
Pablo Jiménez <Pablo Jimnez@DESKTOP-UEAH07A>
2023-05-06 20:55:25 +0200

    dioo acabao

--- Practica2_UF5_Pablo/src/practica2_uf5_pablo/EquacioGrau2.java
+++ Practica2_UF5_Pablo/src/practica2_uf5_pablo/EquacioGrau2.java
@@ -9,38 +9,40 @@ package practica2_uf5_pablo;
  * @author ausias
  */
 public class EquacioGrau2 {
-    
+
     private double a;
     private double b;
     private double c;
     private double sol1;
     private double sol2;
-    
-    public EquacioGrau2(double a, double b, double c) throws PrimerCoeficientZeroException, NoArrelsRealsException{
+
+    public EquacioGrau2(double a, double b, double c) throws PrimerCoeficientZeroException, NoArrelsRealsException  {
         if (a == 0) {
             throw new PrimerCoeficientZeroException("El primer coeficient es cero");
         }
+
         if (b * b < 4 * a * c) {
             throw new NoArrelsRealsException("Error d'arrel");
         }
         this.a = a;
         this.b = b;
         this.c = c;
-        arrels();
+
     }
-    
-    public void arrels() {
+
+    public void arrels(){
+        
         double operacio = b * b - 4 * a * c;
         sol1 = (-b + Math.sqrt(operacio)) / (2 * a);
         sol2 = (-b - Math.sqrt(operacio)) / (2 * a);
     }
-    
+
     public double getSol1() {
         return sol1;
     }
-    
+
     public double getSol2() {
         return sol2;
     }
-    
+
 }
--- Practica2_UF5_Pablo/src/practica2_uf5_pablo/Practica2_UF5_Pablo.java
+++ Practica2_UF5_Pablo/src/practica2_uf5_pablo/Practica2_UF5_Pablo.java
@@ -12,17 +12,14 @@ public class Practica2_UF5_Pablo {
 
     public static void main(String[] args) {
         try {
-            EquacioGrau2 eq = new EquacioGrau2(0, 4, 8);
-            double sol1 = eq.getSol1();
-            double sol2 = eq.getSol2();
-            
-            System.out.println("Primer resultat: " + sol1 + " | Segon resultat: " + sol2);
+            EquacioGrau2 eq = new EquacioGrau2(0, 5, 6);
+            eq.arrels();
+            System.out.println("Solucions: " + eq.getSol1() + ", " + eq.getSol2());
         } catch (PrimerCoeficientZeroException e) {
             System.out.println(e.getMessage());
         } catch (NoArrelsRealsException e) {
             System.out.println(e.getMessage());
         }
-
     }
 
 }
